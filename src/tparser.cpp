98a2228667c8a338ba51f3b9614703a4c50a5e37
ABackerNINI <abacker.nini@foxmail.com>
2024-04-04 03:34:42 -0700

    save

---
+++ src/tparser.cpp
@@ -0,0 +1,11 @@
+#include "tparser.h"
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(int argc, char **argv) {
+    
+
+    return 0;
+}
---
+++ src/tparser.h
@@ -0,0 +1,6 @@
+#ifndef __TEMPLATE_PARSER_H__
+#define __TEMPLATE_PARSER_H__
+
+
+
+#endif // __TEMPLATE_PARSER_H__
