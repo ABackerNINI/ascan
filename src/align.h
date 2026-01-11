79dd2a68c7f71d147173bb56d90f814f73a301c7
ABackerNINI <abacker.nini@foxmail.com>
2026-01-11 06:37:59 -0800

    remove align.cpp

--- src/align.h
+++ src/align.h
@@ -10,20 +10,37 @@ class Align {
       public:
         AlignWrapper(const Align &align, const std::string &word) : align(align), word(word) {}
 
-        std::string to_string() const;
+        std::string to_string() const {
+            std::string str = word;
+            if (align.get_max_length() > word.length()) {
+                size_t n = align.get_max_length() - word.length();
+                for (size_t i = 0; i < n; i++) {
+                    str += " ";
+                }
+            }
+            return str;
+        }
 
-        friend std::ostream &operator<<(std::ostream &os, const AlignWrapper &wrapper);
+        friend std::ostream &operator<<(std::ostream &os, const AlignWrapper &wrapper) {
+            os << wrapper.to_string();
+            return os;
+        }
 
       private:
         const Align &align;
         const std::string &word;
     };
 
-    void add(const std::string &word);
+    void add(const std::string &word) {
+        // print_debug("word: %s\n", word.c_str());
+        if (word.length() > max_length) {
+            max_length = word.length();
+        }
+    }
 
-    size_t get_max_length() const;
+    size_t get_max_length() const { return max_length; }
 
-    AlignWrapper operator()(const std::string &word) const;
+    AlignWrapper operator()(const std::string &word) const { return AlignWrapper(*this, word); }
 
   private:
     size_t max_length = 0;
